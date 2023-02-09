53eb646dc068771c6a260991763320022aee9b3d
Kemail-terminal <sulkemref@gmail.com>
2023-02-09 14:47:21 -0600

    single responsibility principle

--- src/com/cydeo/X.java
+++ src/com/cydeo/X.java
@@ -1,9 +1,15 @@
 package com.cydeo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class X {
 
     public static void main(String[] args) {
 
+
+        List<Integer> list = new ArrayList<>();
+        //list
 
         Z z = new A();
 
---
+++ src/com/cydeo/solid/singleResponsibility/example/good/CRUDService.java
@@ -0,0 +1,28 @@
+package com.cydeo.solid.singleResponsibility.example.good;
+
+public class CRUDService {
+
+    public static void create(){
+        //Create a user
+        //Send it ti database related part
+
+    }
+
+    public static void read(){
+        // Read user
+        // Send user to database related part
+    }
+
+    public static void update(){
+        // Update user
+        // Send user to database related part
+    }
+
+    public static void delete(){
+        // Delete user
+        // Send user to database related part
+    }
+
+
+
+}
