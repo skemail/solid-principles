c1adfdb46caee7d0657946e73c42a2aead85c21d
Kemail-terminal <sulkemref@gmail.com>
2023-02-09 15:44:32 -0600

    open closed principle

---
+++ src/com/cydeo/solid/openClosed/example1/good/DrawShape.java
@@ -0,0 +1,14 @@
+package com.cydeo.solid.openClosed.example1.good;
+
+import java.util.List;
+
+public class DrawShape {
+
+    public void startDrawing(List<Shape> shapes){
+
+        for(Shape shape : shapes){
+            shape.draw();
+        }
+
+    }
+}
